65b78bfbdb73d58635e463e19ead03784a51c47f
Lingmin Zhou <lingmin.zhou@oscarservice.com>
2014-05-21 16:25:44 +0800

    fixed bugs

--- src/main/java/com/phoenixcloud/resources/BookAction.java
+++ src/main/java/com/phoenixcloud/resources/BookAction.java
@@ -75,6 +75,50 @@ public class BookAction {
 	}
 	
 	@POST
+	@Path("uploadFile/{code}/cover/{fileName}")
+	//@Consumes(MediaType.APPLICATION_OCTET_STREAM)
+	@Produces({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
+	public String uploadCover(InputStream fis,
+			@PathParam("code") String code,
+			@PathParam("fileName") String fileName) {
+		
+		JSONObject retObj = new JSONObject();
+		try {
+			fileName = URLDecoder.decode(fileName, "utf-8");
+			code = URLDecoder.decode(code, "utf-8");
+		} catch (UnsupportedEncodingException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+			retObj.put("ret", 1);
+			retObj.put("error", e1.toString());
+		}
+		String bookDir = prop.getProperty("book_file_folder");
+		File folder = new File(bookDir + File.separator + code);
+		if (!folder.exists()) {
+			folder.mkdirs();
+		}
+		
+		OutputStream fos = null;
+		try {
+			fos = new FileOutputStream(new File(bookDir + File.separator + code + File.separator + fileName));
+			byte[] buffer = new byte[1024];
+			int len = 0;
+			while ((len = fis.read(buffer)) != -1) {
+				fos.write(buffer, 0, len);
+			}
+			fos.flush();
+			retObj.put("ret", 0);
+			fis.close();
+			fos.close();
+		} catch (Exception e) {
+			retObj.put("ret", 1);
+			retObj.put("error", e.toString());
+		}
+
+		return retObj.toString();
+	}
+	
+	@POST
 	@Path("test")
 	@Consumes(MediaType.MULTIPART_FORM_DATA)
 	@Produces(MediaType.APPLICATION_JSON)
@@ -115,5 +159,38 @@ public class BookAction {
 				.header("Content-disposition","attachment;filename=\"" + downFileName + "\"")
 				.header("ragma", "No-cache").header("Cache-Control", "no-cache").build();
 	}
+	
+	@GET
+	@Path("downloadFile/{code}/cover/{fileName}")
+	@Produces(MediaType.APPLICATION_OCTET_STREAM)
+	public Response downloadCover(@PathParam("code") String code,
+			@PathParam("fileName") String fileName) {	
+
+		String bookDir = prop.getProperty("book_file_folder");
+		try {
+			fileName = URLDecoder.decode(fileName, "utf-8");
+			code = URLDecoder.decode(code, "utf-8");
+		} catch (UnsupportedEncodingException e1) {
+			// TODO Auto-generated catch block
+			throw new WebApplicationException(404);
+		}
+		File file = new File(bookDir + File.separator + code, fileName);
+		if (!file.exists()) {
+			throw new WebApplicationException(404);
+		}
+		String mt = new MimetypesFileTypeMap().getContentType(file);
+		String downFileName = fileName;
+		try {
+			downFileName = new String(fileName.getBytes(), "ISO-8859-1");
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			MiscUtils.getLogger().info(e.toString());
+		}
+		return Response
+				.ok(file, mt)
+				.header("Content-disposition","attachment;filename=\"" + downFileName + "\"")
+				.header("ragma", "No-cache").header("Cache-Control", "no-cache").build();
+	}
+	
 	
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,3 +1,8 @@
+<%@page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8"%>
+
+<%
+response.addHeader("Access-Control-Allow-Origin", "*"); // used for cross domain access
+%>
 <html>
 <body>
 <h2>Hello World!</h2>
